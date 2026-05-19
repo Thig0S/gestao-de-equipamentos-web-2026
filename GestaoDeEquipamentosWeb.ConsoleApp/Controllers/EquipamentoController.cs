ef2fa166b3d86b4fc850fecca38ca6e7264067c9
thig0S_ <thiagokovalski5@gmail.com>
2026-05-18 21:56:11 -0300

    base do View Listar Equipamento feita

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/EquipamentoController.cs
@@ -0,0 +1,43 @@
+
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
+using GestaoDeEquipamentosWeb.ConsoleApp.Models;
+using GestaoDeEquipamentosWeb.ConsoleApp.ModuloEquipamento;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers
+{
+    public class EquipamentoController : Controller
+    {
+        private readonly IRepositorio<Equipamento> repositorioEquipamento;
+
+        public EquipamentoController()
+        {
+            ContextoJson contexto = new();
+            contexto.Carregar();
+
+            repositorioEquipamento = new RepositorioEquipamentoEmArquivo(contexto);
+        }
+
+        public ActionResult Listar()
+        {
+            List<Equipamento> equipamentos = repositorioEquipamento.SelecionarTodos();
+
+            List<ListarEquipamentosViewModel> listarVm = new List<ListarEquipamentosViewModel>();
+
+            foreach (Equipamento e in equipamentos)
+            {
+                ListarEquipamentosViewModel viewModel = new ListarEquipamentosViewModel(
+                    e.Id,
+                    e.Nome,
+                    e.PrecoAquisicao,
+                    e.DataFabricacao,
+                    e.Fabricante.Nome
+                );
+                listarVm.Add(viewModel);
+            }
+
+            return View();
+        }
+    }
+}
